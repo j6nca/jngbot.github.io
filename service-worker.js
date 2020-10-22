e4588fb26a4310158d0daa0dfd9b0776c89f6453
jngdesign <jng.cad@gmail.com>
2020-10-22 00:48:07 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cdd8d82303bf73149b3340e313915b96.js"
+  "/precache-manifest.50daa8d225404651b097b91af8f0533a.js"
 );
 
 self.addEventListener('message', (event) => {
