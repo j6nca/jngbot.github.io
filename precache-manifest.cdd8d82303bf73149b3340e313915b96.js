04d9f7461d541bc73a9334471eb98aa3a885eb50
jngdesign <jng.cad@gmail.com>
2020-10-21 23:22:43 -0400

    Updates

---
+++ precache-manifest.cdd8d82303bf73149b3340e313915b96.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d37fb352e90d692e1d95d520e414f6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73575fb1d6c40eff4854",
+    "url": "/static/css/main.54829da0.chunk.css"
+  },
+  {
+    "revision": "6079d42e9aa5d55c88c1",
+    "url": "/static/js/2.31227656.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.31227656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73575fb1d6c40eff4854",
+    "url": "/static/js/main.aeaa71c2.chunk.js"
+  },
+  {
+    "revision": "142d6829b916f62f2bb9",
+    "url": "/static/js/runtime-main.046262a6.js"
+  },
+  {
+    "revision": "31588a77875608df4e602ea521366805",
+    "url": "/static/media/about.31588a77.mp4"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
